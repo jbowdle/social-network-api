e7e923c32695a20284c00e9e0c5166ef179d710c
jbowdle <johnwbowdle@gmail.com>
2023-06-07 23:17:16 -0500

    created update user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,6 +34,24 @@ module.exports = {
       return res.status(500).json(err);
     }
   },
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: "No users found" });
+      }
+
+      res.status(200).json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
   async deleteUser(req, res) {
     try {
       const user = await User.findOneAndRemove({ _id: req.params.userId })
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,10 +3,11 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUser,
   deleteUser 
 } = require("../../controllers/userController");
 
 router.route("/").get(getUsers).post(createUser);
-router.route("/:userId").get(getSingleUser).delete(deleteUser);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
