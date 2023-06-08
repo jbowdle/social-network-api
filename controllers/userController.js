e8e62139e784ba74eb618e297651671b11d93f4a
jbowdle <johnwbowdle@gmail.com>
2023-06-07 23:13:04 -0500

    added create user and delete user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,4 +25,27 @@ module.exports = {
       return res.status(500).json(err);
     }
   },
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.status(200).json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndRemove({ _id: req.params.userId })
+
+      if (!user) {
+        return res.status(404).json({ message: "No users found" });
+      }
+
+      res.status(200).json({ message: `User ${user.username} deleted`});
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const { 
   getUsers,
-  getSingleUser 
+  getSingleUser,
+  createUser,
+  deleteUser 
 } = require("../../controllers/userController");
 
-router.route("/").get(getUsers);
-router.route("/:userId").get(getSingleUser);
+router.route("/").get(getUsers).post(createUser);
+router.route("/:userId").get(getSingleUser).delete(deleteUser);
 
 module.exports = router;
